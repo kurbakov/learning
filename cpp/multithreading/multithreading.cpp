9a39e127551c094b8d995b5c78f6d85d30954af5
Dmytro Kurbakov <kurbakov@users.noreply.github.com>
2016-11-20 20:22:40 +0100

    Update and rename print_function.cpp to multithreading.cpp

--- cpp/multithreading/multithreading.cpp
+++ cpp/multithreading/multithreading.cpp
@@ -1,15 +1,18 @@
 #include "iostream"
 #include "thread"
 
-// Define number of threads
+// Define global variables
 static const int N_THREADS = 4;
+std::vector<int> VECTOR;
+std::mutex M;
 
-// test void function
+/*=================================================*/
+// test print function
 void print(std::string msg){
     std::cout << msg << std::endl;
 }
 
-void run_in_single_thread(std::string msg){
+void run_in_single_thread_print(std::string msg){
     std::thread t1(print, msg);
     t1.join();
     return;
@@ -24,6 +27,25 @@ void run_parallel_print(std::string msg){
         t[i] = std::thread(print, "a lot of threads are here!");
 
     // join all threads
+    for (int i = 0; i < N_THREADS; ++i)
+        t[i].join();
+
+    return;
+}
+
+/*=================================================*/
+// test function with changing some element in vector
+void compute(int i){
+    M.lock();
+    VECTOR[i] = VECTOR[i]*VECTOR[i] + 100;
+    M.unlock();
+}
+
+void run_parallel_compute(){
+    std::thread t[N_THREADS];
+    for (int i = 0; i < N_THREADS; ++i)
+        t[i] = std::thread(compute, i);
+
     for (int i = 0; i < N_THREADS; ++i)
         t[i].join();
 
