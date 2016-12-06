fc5ee4f885034b9e3a7480c08f95150547107a36
Dmytro Kurbakov <kurbakov@users.noreply.github.com>
2016-12-06 21:38:33 +0100

    Create graph_bfs.cpp

---
+++ cpp/algorithms/other/graph_bfs.cpp
@@ -0,0 +1,79 @@
+#include<iostream>
+#include <list>
+
+class Graph
+{
+    int size;    
+    std::list<int> *adj;    
+public:
+    Graph(int V); 
+    void addEdge(int, int); 
+    void BFS(int); 
+};
+
+Graph::Graph(int V)
+{
+    this->size = V;
+    adj = new std::list<int>[V];
+}
+
+void Graph::addEdge(int from, int to)
+{
+    adj[from].push_back(to); // Add w to v’s list.
+}
+
+void Graph::BFS(int start)
+{
+    // Mark all the vertices as not visited
+    bool *visited = new bool[size];
+    for(int i = 0; i < size; i++)
+        visited[i] = false;
+
+    // Create a queue for BFS
+    std::list<int> queue;
+
+    // Mark the current node as visited and enqueue it
+    visited[start] = true;
+    queue.push_back(start);
+
+    // 'i' will be used to get all adjacent vertices of a vertex
+    std::list<int>::iterator i;
+
+    while(!queue.empty())
+    {
+        // Dequeue a vertex from queue and print it
+        start = queue.front();
+        std::cout << start << " ";
+        queue.pop_front();
+
+        // Get all adjacent vertices of the dequeued vertex s
+        // If a adjacent has not been visited, then mark it visited
+        // and enqueue it
+        for(i = adj[start].begin(); i != adj[start].end(); ++i)
+        {
+            if(!visited[*i])
+            {
+                visited[*i] = true;
+                queue.push_back(*i);
+            }
+        }
+    }
+}
+
+
+int main()
+{
+    Graph g(4);
+    g.addEdge(0, 1);
+    g.addEdge(0, 2);
+    g.addEdge(1, 2);
+    g.addEdge(2, 0);
+    g.addEdge(2, 3);
+    g.addEdge(3, 3);
+
+    std::cout << "The result of BFS(2):" << std::endl;
+    g.BFS(2);
+
+    return 0;
+}
+
