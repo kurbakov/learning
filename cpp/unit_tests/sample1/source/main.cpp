ad0c3665464b5610ffa8970904f1e38aa9aef316
dmytro.kurbakov@deliveryhero.com <alessandro_parma@hotmail.it>
2017-01-08 20:10:09 +0100

    google test framework

---
+++ cpp/unit_tests/sample1/source/main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include "my_math.h"
+
+int main(){
+    My_Math m;
+    std::cout << m.addition(20, 20) << std::endl;
+    return 0;
+}
---
+++ cpp/unit_tests/sample1/source/my_math.h
@@ -0,0 +1,15 @@
+#ifndef MY_MATH_H
+#define MY_MATH_H
+
+class My_Math {
+public:
+    My_Math(){};
+
+    int addition(int l, int r);
+    int subtraction(int l, int r);
+    int multiply(int l, int r);
+    int divide(int l, int r);
+    bool equal(int l, int r);
+};
+
+#endif
