96e746f500652be1f91714be777d0aafd70aef53
dmytro.kurbakov@deliveryhero.com <dmytro.kurbakov@deliveryhero.com>
2017-02-16 22:02:06 +0100

    created a project template with gtets

---
+++ cpp/unit_tests/sample2/scr/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include "scr/my_math.h"
+
+int main(){
+	// std::cout << "hello world!" << std::endl;
+    My_Math m;
+    std::cout << m.addition(20, 20) << std::endl;
+    return 0;
+}
