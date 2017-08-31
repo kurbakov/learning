a367d5a91e46acff9fea2ffca0b012ba97ced989
kurbakovDH <dmytro.kurbakov@deliveryhero.com>
2017-08-31 10:52:23 +0200

    utests

---
+++ UnitTests/scr/my_math.cpp
@@ -0,0 +1,18 @@
+#include "my_math.h"
+
+int My_Math::addition(int l, int r){
+	return l+r;
+}
+int My_Math::subtraction(int l, int r){
+	return l-r;
+}
+int My_Math::multiply(int l, int r){
+	return l*r;
+}
+int My_Math::divide(int l, int r){
+	return l/r;
+}
+
+bool My_Math::equal(int l, int r){
+	return l==r;
+}
